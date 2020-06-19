043bd3427b5a8080e0b468a9812e230151e96f3b
sarvesh khandelwal <sarveshk21122001@gmail.com>
2020-06-19 15:18:12 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
-const path = require('path');
+const path= require('path');
 
 //import the route
 const posts=require('./routes/api/posts');
@@ -30,7 +30,7 @@ app.use('/api/users',users);
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'));
     
-    app.get('*',(res.req)=>{
+    app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
